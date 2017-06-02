b281081763595dc9f00d67400d423531775841f9
Michael Lee <ckmichael8@gmail.com>
2017-06-03 03:15:56 +0800

    https

--- server.js
+++ server.js
@@ -13,17 +13,18 @@ app.get('/api/getSbaProblem', function (req, res) {
             res.send("request too large");
         } else {
             var resarr = [];
+            var ddata = JSON.parse(JSON.stringify(data));
             for (var i = 0; i < n; i++) {
-                var ranindex = Math.floor(Math.random() * data.content.length);
+                var ranindex = Math.floor(Math.random() * ddata.content.length);
                 var obj = {};
-                obj.org = data.content[ranindex].whole;
-                if (data.content[ranindex].meaning) {
-                    obj.mean = data.content[ranindex].meaning;
+                obj.org = ddata.content[ranindex].whole;
+                if (ddata.content[ranindex].meaning) {
+                    obj.mean = ddata.content[ranindex].meaning;
                 } else {
                     obj.mean = "";
                 }
-                var testarr = data.content[ranindex].content.split(new RegExp(arrToRegex(data.symbols), 'g'));
-                console.log(arrToRegex(data.symbols));
+                var testarr = ddata.content[ranindex].content.split(new RegExp(arrToRegex(ddata.symbols), 'g'));
+                console.log(arrToRegex(ddata.symbols));
                 while (testarr[testarr.length - 1] == "") {
                     testarr.pop();
                 }
@@ -36,11 +37,12 @@ app.get('/api/getSbaProblem', function (req, res) {
                     obj.a.push(testarr.splice(Math.floor(Math.random() * testarr.length), 1)[0]);
                     numberleft--;
                 }
-                obj.q = data.content[ranindex].content;
+                obj.q = ddata.content[ranindex].content;
                 for (var j = 0; j < obj.a.length; j++) {
                     obj.q = obj.q.replace(obj.a[j], "{" + j.toString() + "}");
                 }
                 resarr.push(obj);
+                ddata.splice(ranindex, 1);
             }
             res.send(JSON.stringify(resarr));
         }
