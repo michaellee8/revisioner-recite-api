16165650583e7106bac768388c2d70efd200209c
Michael Lee <ckmichael8@gmail.com>
2017-06-03 02:42:44 +0800

    added

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/api/getSbaProblem', function (req, res) {
     }
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
     var host = server.address().address
     var port = server.address().port
 
